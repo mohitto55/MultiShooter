e0b689a2845bcf026c7f4fc3a45027989c85e594
mohitto55 <mohitto5865@gmail.com>
2024-04-24 09:59:00 +0900

    [Add] Project Generation
    언리얼 프로젝트 기본생성

---
+++ Source/MultiShooter/MultiShooterGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MultiShooterGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AMultiShooterGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AMultiShooterGameMode();
+};
+
+
+
